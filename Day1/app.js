34b49e92720793afd37db598b39e7650e06be5c4
DeathDealer007 <ankit19kumar2004@gmail.com>
2025-06-16 13:47:12 +0530

    MERN

---
+++ Day1/app.js
@@ -0,0 +1,15 @@
+// window.console.log("Hello from JS!")
+// console.log(age);
+// //let, const or var
+// var age=20;
+// console.log(age);
+
+// var age=30;
+// console.log(age);
+
+// console.log(age+10);
+
+//---------------------------------
+console.log();
+let age=22
+console.log(age);
